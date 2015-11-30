b2d3ffeb5409ba0b985edf9ac5e52eb333fb53a7
windyknight <jjslopez110@gmail.com>
2015-11-30 12:49:32 +0800

    Warrior class is building! Item class is complete! Yay!

--- BattleSim/src/battlesim/Item.java
+++ BattleSim/src/battlesim/Item.java
@@ -10,5 +10,61 @@ package battlesim;
  * @author Jay Lopez
  */
 public class Item {
+    private String name;
+    private int tough;
+    private int dex;
+    private int smart;
+    private double armor;
+    private double hp;
+    private double regen;
+    private double energy;
+    private double ias;
     
+    Item(String name, int tough, int dex, int smart, double armor, double hp, double regen, double energy, double ias) {
+        this.name = name;
+        this.tough = tough;
+        this.dex = dex;
+        this.smart = smart;
+        this.armor = armor;
+        this.hp = hp;
+        this.regen = regen;
+        this.energy = energy;
+        this.ias = ias;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getTough() {
+        return tough;
+    }
+    
+    public int getDex() {
+        return dex;
+    }
+    
+    public int getSmart() {
+        return smart;
+    }
+    
+    public double getArmor() {
+        return armor;
+    }
+    
+    public double getHp() {
+        return hp;
+    }
+    
+    public double getRegen() {
+        return regen;
+    }
+    
+    public double getEnergy() {
+        return energy;
+    }
+    
+    public double getIas() {
+        return ias;
+    }
 }
--- BattleSim/src/battlesim/Warrior.java
+++ BattleSim/src/battlesim/Warrior.java
@@ -18,28 +18,124 @@ public class Warrior {
     //private base stats//
     private final double baseHp = 150;
     private final double baseEnergy = 0;
-    private int baseTough;
-    private int baseDex;
-    private int baseSmart;
+    private int baseTough; //hp and regen
+    private int baseDex; //armor and ias
+    private int baseSmart; //energy
     private int incTough;
     private int incDex;
     private int incSmart;
     private final double baseRegen = 0.25;
     private double baseAttackTime;
+    private double baseArmor;
     //end private base stats//
     private double minDamage;
     private double maxDamage;
     private double currentHp;
     private double maxHp;
     private double energy;
-    private double armor;
+    private double trueArmor;
     private double incAttackSpd;
     private ArrayList<Item> items;
     //final stats to be used in combat//
-    private int tough;
-    private int dex;
-    private int smart;
+    private int tough; //hp and regen
+    private int dex; //armor and ias
+    private int smart; //energy
     private double regen;
     private double damageMitigation;
     private double attackCooldown;
+    private boolean alive;
+    
+    Warrior(String name, Type type, int baseTough, int incTough, int baseDex, int incDex, int baseSmart, int incSmart, double baseArmor, double minDamage, double maxDamage, double baseAttackTime) {
+        this.name = name;
+        this.type = type;
+        this.baseTough = baseTough;
+        this.incTough = incTough;
+        this.baseDex = baseDex;
+        this.incDex = incDex;
+        this.baseSmart = baseSmart;
+        this.incSmart = incSmart;
+        this.baseArmor = baseArmor;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+        this.baseAttackTime = baseAttackTime;
+        this.items = new ArrayList<>();
+        this.alive = true;
+    }
+    
+    
+    public void computeNewStats() {
+        tough = baseTough + ((level - 1) * incTough);
+        for (Item a : items) {
+            tough += a.getTough();
+        }
+        
+        smart = baseSmart + ((level - 1) * incSmart);
+        for (Item a : items) {
+            smart += a.getSmart();
+        }
+        
+        dex = baseDex + ((level - 1) * incDex);
+        for (Item a : items) {
+            dex += a.getDex();
+        }
+        
+        regen = baseRegen + (tough * 0.03);
+        for (Item a : items) {
+            regen += a.getRegen();
+        }
+        
+        maxHp = baseHp + (tough * 19);
+        for (Item a : items) {
+            maxHp += a.getHp();
+        }
+        
+        energy = baseEnergy + (smart * 13);
+        for (Item a : items) {
+            energy += a.getEnergy();
+        }
+        
+        trueArmor = baseArmor + (0.14 * dex);
+        for (Item a : items)  {
+            trueArmor += a.getArmor();
+        }
+        
+        incAttackSpd = dex;
+        for (Item a : items) {
+            incAttackSpd += a.getIas();
+        }
+        
+        damageMitigation = (0.06 * trueArmor) / (1 + (0.06 * trueArmor));
+        attackCooldown = ((int)(baseAttackTime / (1 + (incAttackSpd / 100)) * 100)) / 100.00;
+    }
+    
+    public void levelUp() {
+        level += 1;
+    }
+    
+    public void fullHeal() {
+        currentHp = maxHp;
+    }
+    
+    public void partialHeal(double heal) {
+        currentHp = currentHp + heal > maxHp ? maxHp : currentHp + heal;
+    }
+    
+    public void takeItem (Item item) {
+        items.add(item);
+    }
+    
+    public void dropItem (Item item) {
+        if (items.indexOf(item) > -1) {
+            items.remove(item);
+        }
+    }
+    
+    public void takeDamage(double damage) {
+        currentHp -= damage;
+        if (currentHp <= 0) {
+            alive = false;
+        }
+    }
+    
+    //more stuff
 }
